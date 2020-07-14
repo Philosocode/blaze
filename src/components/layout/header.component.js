68751b6646b972334d219f7372413c7c6b3fa53b
Tam Le <philosocode@gmail.com>
2020-07-13 20:10:19 -0600

    Change logo

--- src/components/layout/header.component.js
+++ src/components/layout/header.component.js
@@ -1,37 +1,29 @@
 import React from "react";
-import { graphql, useStaticQuery, Link } from "gatsby";
-import Image from "gatsby-image";
+import { Link } from "gatsby";
 
+import BlazeLogo from "../../icons/blaze.svg";
 import { navLinks } from "../../shared/data.shared";
 
 const Header = () => { 
-  const data = useStaticQuery(graphql`
-    query {
-      logoImage: file(relativePath: { eq: "gatsby-icon.png" }) {
-        ...fluidImage
-      }
-    }
-  `);
-
   function renderNavLinks() {
     return navLinks.map(navLink => (
-      <Link
-        className="c-heading c-heading--condensed c-header__nav-link" 
-        to={navLink.path}
-      >{navLink.name}</Link>
+      <li className="c-header__nav-item">
+        <Link
+          className="c-heading c-heading--condensed c-header__nav-link" 
+          to={navLink.path}
+        >{navLink.name}</Link>
+      </li>
     ));
   }
 
   return (
     <header className="c-header">
       <Link to="#hero" aria-label="Home">
-        <Image className="c-header__logo" fluid={data.logoImage.childImageSharp.fluid} alt="Logo" />
+        <BlazeLogo className="c-header__logo" />
       </Link>
       <nav role="navigation">
         <ul className="c-header__nav-list">
-          <li className="c-header__nav-item">
-            {renderNavLinks()}
-          </li>
+          {renderNavLinks()}
         </ul>
       </nav>
     </header>
